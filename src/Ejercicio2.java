6fb526b3d250684a730bc5cd7eca1a7b05c86ac2
Contreras-Carbajal-Marco-Antonio <20253DS010@utez.edu.mx>
2026-09-25 12:37:14 -0600

    Continuidad

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
+import java.util.Random;
+
 public class Ejercicio2 {
     static Scanner cin = new Scanner(System.in);
+    static Random random = new Random();
     public static void main(String[] args) {
-        int[][] mapa = new int[5][5];
+        String[][] mapa = new String[5][5];
         String vacio = "⬜";
         String robot = "🤖";
         String enemigo = "👹";
         String[] vida = {"💙","💙","💙"};
+        mapa[0][0] = robot;
 
-        int columna,fila;
+        int columna=0,fila=0;
 
         do{
             for(int i=0; i<mapa.length; i++){
@@ -17,8 +21,49 @@ public class Ejercicio2 {
                 }
                 System.out.println();
             }
-        
+            for(int i = 1; i<5; i++){
+                int filaEnemigo = random.nextInt(5);
+                int columnaEnemigo = random.nextInt(5);
+                if(mapa[filaEnemigo][columnaEnemigo] == vacio){
+                    mapa[filaEnemigo][columnaEnemigo] = enemigo;
+                }     
+            }
             
+            System.out.println("Ingrese una tecla para moverte:");
+            System.out.println("w: arriba");
+            System.out.println("s: abajo");
+            System.out.println("a: izquierda");
+            System.out.println("d: derecha");
+            String tecla = cin.nextLine().toLowerCase();
+            switch(tecla){
+                case "w":
+                    if(mapa[fila-1][columna] == vacio){
+                        mapa[fila][columna] = vacio;
+                        mapa[fila-1][columna] = robot;
+                    }
+                    break;
+                case "s":
+                    if(mapa[fila+1][columna] == vacio){
+                        mapa[fila][columna] = vacio;
+                        mapa[fila+1][columna] = robot;
+                    }
+                    break;
+                case "a":
+                    if(mapa[fila][columna-1] == vacio){
+                        mapa[fila][columna] = vacio;
+                        mapa[fila][columna-1] = robot;
+                    }
+                    break;
+                case "d":
+                    if(mapa[fila][columna+1] == vacio){
+                        mapa[fila][columna] = vacio;
+                        mapa[fila][columna+1] = robot;
+                    }
+                    break;
+                default:
+                    System.out.println("Tecla no valida");
+                    break;
+            }
         } while (mapa[5][5] == robot || vida[0] == "💔");
     }
 }
