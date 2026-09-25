91ed2399692081b32fc22d1941f1f021de8e6c59
Contreras-Carbajal-Marco-Antonio <20253DS010@utez.edu.mx>
2026-09-25 13:23:13 -0600

    12

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -3,36 +3,45 @@ import java.util.Random;
 
 public class Ejercicio2 {
     static Scanner cin = new Scanner(System.in);
-    static Random random = new Random();
+
     public static void main(String[] args) {
         String[][] mapa = new String[5][5];
         String vacio = "⬜";
         String robot = "🤖";
-        String enemigo = "👹";
+        String enemigo = "👺s";
         int vida = 3;
         int fila = 0;
+
         int columna = 0;
         int metaFila = mapa.length - 1;
         int metaColumna = mapa[0].length - 1;
+        mapa[4][4] = "🏁";
 
-        for (int i = 0; i < mapa.length; i++) {
-            for (int j = 0; j < mapa[i].length; j++) {
-                mapa[i][j] = vacio;
-            }
-        }
+
         mapa[fila][columna] = robot;
 
+        Random random = new Random();
         int enemigosColocados = 0;
-        while (enemigosColocados < 4) {
+
+        void laberinto() {
+            for (int i = 0; i < mapa.length; i++) {
+                for (int j = 0; j < mapa[i].length; j++) {
+                    mapa[i][j] = vacio;
+                    mapa[4][4] = "🏁";
+                }
+            }
+        }
+
+        do {
             int filaEnemigo = random.nextInt(mapa.length);
             int columnaEnemigo = random.nextInt(mapa[0].length);
-            boolean esInicio = filaEnemigo == 0 && columnaEnemigo == 0;
-            boolean esMeta = filaEnemigo == metaFila && columnaEnemigo == metaColumna;
-            if (!esInicio && !esMeta && mapa[filaEnemigo][columnaEnemigo] == vacio) {
+            boolean esInicio = (filaEnemigo == 0 && columnaEnemigo == 0);
+            boolean esMeta = (filaEnemigo == metaFila && columnaEnemigo == metaColumna);
+            if (!esInicio && !esMeta && mapa[filaEnemigo][columnaEnemigo].equals(vacio)) {
                 mapa[filaEnemigo][columnaEnemigo] = enemigo;
                 enemigosColocados++;
             }
-        }
+        } while(enemigosColocados < 4);
 
         do {
             for (int i = 0; i < mapa.length; i++) {
@@ -70,8 +79,7 @@ public class Ejercicio2 {
                     continue;
             }
 
-            if (nuevaFila < 0 || nuevaFila >= mapa.length
-                    || nuevaColumna < 0 || nuevaColumna >= mapa[nuevaFila].length) {
+            if (nuevaFila < 0 || nuevaFila >= mapa.length || nuevaColumna < 0 || nuevaColumna >= mapa[nuevaFila].length) {
                 System.out.println("Movimiento invalido");
             } else if (mapa[nuevaFila][nuevaColumna] == enemigo) {
                 vida--;
@@ -80,6 +88,19 @@ public class Ejercicio2 {
                 fila = nuevaFila;
                 columna = nuevaColumna;
                 System.out.println("Chocaste con un enemigo. Vidas: " + vida);
+                enemigosColocados = 0;
+
+                do {
+                    int filaEnemigo = random.nextInt(mapa.length);
+                    int columnaEnemigo = random.nextInt(mapa[0].length);
+                    boolean esInicio = (filaEnemigo == 0 && columnaEnemigo == 0);
+                    boolean esMeta = (filaEnemigo == metaFila && columnaEnemigo == metaColumna);
+                    if (!esInicio && !esMeta && mapa[filaEnemigo][columnaEnemigo].equals(vacio)) {
+                        mapa[filaEnemigo][columnaEnemigo] = enemigo;
+                        enemigosColocados++;
+                    }
+                } while (enemigosColocados < 4);
+                
             } else if (mapa[nuevaFila][nuevaColumna] == vacio) {
                 mapa[fila][columna] = vacio;
                 mapa[nuevaFila][nuevaColumna] = robot;
